117b6a8fd6f2711286396f3220ee7f5c046260b6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-01-14 19:24:15 +0100

    nix: simplify things
    DRY

--- src/lib.nix
+++ src/lib.nix
@@ -1,13 +1,16 @@
-{ nixpkgs }:
-
-let
-  CIs = map (x: import x) (import ./CI { inherit (nixpkgs) lib; }).imports;
-  ci = nixpkgs.lib.lists.findSingle (x: x.isDetected) (throw "Not in CI") (throw "Not in CI") CIs;
-  inCI = if false == (nixpkgs.lib.lists.findSingle (x: x.isDetected) false false CIs) then false else true;
-in
-{
-  inherit ci inCI;
-
-  notInCI = if (true == inCI) then false else true;
-  find = default: if (true == inCI) then ci else default;
+{nixpkgs}: let
+  ci = nixpkgs.lib.lists.findSingle (x: x.isDetected) (throw "Not in CI") (throw "Not in CI") (map (x: import x) (import ./CI {inherit (nixpkgs) lib;}).imports);
+in {
+  inCI =
+    if tryEval ci
+    then false
+    else true;
+  notInCI =
+    if inCI
+    then false
+    else true;
+  find = default:
+    if (true == inCI)
+    then ci
+    else default;
 }
