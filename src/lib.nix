3f4294594f34a8f3be3ab63a5694bd079df16362
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-01-15 18:56:40 +0100

    add forgotten `ci` output

--- src/lib.nix
+++ src/lib.nix
@@ -1,14 +1,18 @@
 {nixpkgs}: let
   ci = nixpkgs.lib.lists.findSingle (x: x.isDetected) (throw "Not in CI") (throw "Not in CI") (map (x: import x) (import ./CI {inherit (nixpkgs) lib;}).imports);
 in rec {
+  inherit ci;
+
   inCI =
-    if ((builtins.tryEval ci) == { success = false; value = false; })
+    if
+      ((builtins.tryEval ci)
+        == {
+          success = false;
+          value = false;
+        })
     then false
     else true;
-  notInCI =
-    if inCI
-    then false
-    else true;
+  notInCI = !inCI;
   find = default:
     if (true == inCI)
     then ci
